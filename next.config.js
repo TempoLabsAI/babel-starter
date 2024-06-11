1a063e4f78d7c5cbe77371f55c0776ee7f524df9
petergok <petergok@hotmail.com>
2024-06-11 16:30:17 -0400

    Switch to babel, npm

--- next.config.js
+++ next.config.js
@@ -6,10 +6,4 @@ const nextConfig = {
   },
 };
 
-if (process.env.NEXT_PUBLIC_TEMPO) {
-  nextConfig["experimental"] = {
-    swcPlugins: [[require.resolve("tempo-devtools/swc"), {}]],
-  };
-}
-
 module.exports = nextConfig;
